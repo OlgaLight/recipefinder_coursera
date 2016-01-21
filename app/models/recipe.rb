e1d8f87a828df22a554525987b8b4a9443e3cd17
OlgaLight <marshak.oggy@gmail.com>
2016-01-21 13:36:58 +0200

    add gems for deploying

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,17 @@
-
-require 'httparty'
-
-class Recipe < ActiveRecord::Base
+# FOOD2FORK_KEY = 'http://food2fork.com/api/search?key=22c0e11125d21a6e447201d26f7cf55f&q=shredded%20chicken'
+class Recipe
   include HTTParty
 
-  key = ENV['FOOD2FORK_KEY']
-  host_port = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
-  base_uri "http://#{host_port}/api"
-  default_params key: key
+  FOOD2FORK_KEY = '22c0e11125d21a6e447201d26f7cf55f'
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+
+  default_params key: FOOD2FORK_KEY,
+                 fields: "image_url, title, social_rank",
+                 q: "search"
   format :json
 
-  def self.for(search)
-    get('/search', query: { q: search }, :verify => false)['recipes']
+  def self.for (key_value)
+    get("/search", query: {q: key_value})["recipes"]
   end
-
 end
